8cc20049cc90df7f7e220f4dc662ee55ceb098aa
WILLCS84 <will.cesar.84@gmail.com>
2025-09-08 13:47:17 -0300

    ajustes nas configs

--- .prettierrc.js
+++
@@ -1,6 +0,0 @@
-export const semi = true;
-export const trailingComma = 'all';
-export const singleQuote = true;
-export const printWidth = 80;
-export const tabWidth = 2;
-export const endOfLine = 'auto';
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,52 +1,42 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 export default [
-  {
-    ignores: ['prettierrc.js', 'eslint.config.mjs', 'tsconfig.json'],
-  },
-  // Configurações padrão do ESLint
+  // Regras básicas JS
   pluginJs.configs.recommended,
 
-  // Configurações e regras do TypeScript
+  // Regras TypeScript
   ...tseslint.configs.recommended,
 
-  // Suas regras e configurações personalizadas
+  // Desativa regras conflitantes com Prettier
+  prettierConfig,
+
+  // Configurações personalizadas para TS
   {
     files: ['**/*.ts'],
     languageOptions: {
-      ecmaVersion: 11,
+      ecmaVersion: 2024,
       sourceType: 'module',
-      // Substitui o "env" para definir variáveis globais
       globals: {
         ...globals.browser,
         ...globals.node,
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly',
-        module: 'readonly',
       },
       parser: tseslint.parser,
       parserOptions: {
         project: './tsconfig.json',
       },
     },
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
     rules: {
       '@typescript-eslint/no-empty-function': 'off',
       semi: ['error', 'always'],
       quotes: ['error', 'single'],
-      // Regra para integração com o Prettier
-      'prettier/prettier': 'error',
-    },
-  },
-
-  // Configuração para o plugin do Prettier
-  {
-    plugins: {
-      prettier: (await import('eslint-plugin-prettier')).default,
-    },
-    rules: {
-      'prettier/prettier': 'error',
+      'prettier/prettier': 'error', // Prettier manda na formatação
     },
   },
 ];
